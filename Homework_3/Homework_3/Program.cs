f92f50df29c548436de55faab0cfe7880e0ed28d
Alina Petrenko <petrenkoaalina@gmail.com>
2021-11-01 17:35:37 +0200

    Created arrays

--- Homework_3/Homework_3/Program.cs
+++ Homework_3/Homework_3/Program.cs
@@ -13,7 +13,59 @@ namespace Homework_3
         /// <param name="args">console call params.</param>
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Please, write rang of array");
+            int value = int.Parse(Console.ReadLine());
+            int[] array = new int[value];
+            Random rand = new Random();
+            int even = 0, odd = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(1, 26);
+                if (array[i] % 2 == 0)
+                {
+                    even++;
+                }
+                else
+                {
+                    odd++;
+                }
+            }
+
+            Console.WriteLine("array");
+            foreach (int i in array)
+            {
+                Console.WriteLine(i);
+            }
+
+            int[] evenArray = new int[even];
+            int[] oddArray = new int[odd];
+            int j = 0;
+            int k = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    evenArray[j] = array[i];
+                    j++;
+                }
+                else
+                {
+                    oddArray[k] = array[i];
+                    k++;
+                }
+            }
+
+            Console.WriteLine("even");
+            foreach (int i in evenArray)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("odd");
+            foreach (int i in oddArray)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
